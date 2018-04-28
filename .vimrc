1305ee27c0866aabda0a35371559346caebe5255
allenwhale <allencat850502@gmail.com>
2018-04-28 08:35:40 +0800

    init commit

---
+++ .vimrc
@@ -0,0 +1,57 @@
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+set rtp+=~/.vim/bundle/vundle/
+call vundle#rc()
+Plugin 'gmarik/vundle'
+Plugin 'tpope/vim-fugitive'
+Plugin 'L9'
+Plugin 'Lokaltog/vim-easymotion'
+Plugin 'scrooloose/nerdtree'
+Plugin 'jistr/vim-nerdtree-tabs'
+Plugin 'bling/vim-airline'
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'bigzhu/html5.vim'
+Plugin 'scrooloose/nerdcommenter'
+Plugin 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+filetype plugin indent on     " required
+
+"airline config
+set laststatus=2
+let g:airline_powerline_fonts=1
+let g:airline#extensions#tabline#enabled=1
+let g:airline#extensions#tabline#buffer_nr_show=1
+
+"youcompltememe config
+let g:ycm_global_ycm_extra_conf = '~/.vim/.ycm_extra_conf.py'
+let g:ycm_enable_diagnostic_signs = 0
+let g:ycm_key_invoke_completion = '<c-\>' 
+set completeopt=menuone
+
+"vim-easymotion config
+map  / <Plug>(easymotion-sn)
+omap / <Plug>(easymotion-tn)
+map  n <Plug>(easymotion-next)
+map  N <Plug>(easymotion-prev)
+
+set t_Co=256
+set term=screen-256color
+set number
+map <F5> :NERDTreeTabsToggle <CR>
+
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set mouse=a
+
+
+set wrap
+set showcmd
+colorscheme torte
+
+autocmd BufNewFile,BufRead *.vue set filetype=html
+
+map <F12> :!gist-paste --raw % <CR>
+
+syntax on
---
+++ env.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+ZSHRC="./.zshrc"
+VIMRC="./.vimrc"
+YCM_CONF="./.ycm_extra_conf.py"
+APT_PACKAGE="\
+    zsh \
+    tmux \
+    python3 \
+    python3-dev \
+    python3-pip \
+    python \
+    python-dev \
+    python-pip \
+    vim \
+    curl \
+    git \
+    gist \
+    build-essential \
+    make \
+    cmake \
+    htop \
+    iotop \
+    iftop \
+    gcc \
+    g++ \
+    bison"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y $APT_PACKAGE
+
+#zsh setup
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+zsh -c 'source ~/.zshrc; git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions'
+zsh -c 'source ~/.zshrc; git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting'
+cp $ZSHRC ~/.zshrc
+sudo chsh -s `which zsh` `whoami`
+
+
+#docker setup
+#curl -fsSL get.docker.com | sh
+#sudo usermod -aG docker `whoami`
+
+#nvm setup
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install v10.0.0
+
+#gvm setup
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source  ~/.gvm/scripts/gvm
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+export CGO_ENABLE=0
+gvm install go1.9
+gvm use go1.9 --default
+
+#vim setup
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+cp $VIMRC ~/.vimrc
+cp $YCM_CONF ~/.vim/
+vim +PluginInstall +GoInstallBinaries +qall
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.py --clang-completer --go-completer
